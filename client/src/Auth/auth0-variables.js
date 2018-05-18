343cca20633c580c444cdb718424dc173fc37d94
KentsCode <kentmbox42@yahoo.com>
2018-05-18 15:54:32 -0500

    updated a route

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'dailyspecial.auth0.com',
   clientId: 'jmK66lMH3jMwrdxi33iamKGLzqgQZANp',
-  callbackUrl: '/callback'
+  callbackUrl: 'https://desolate-fjord-64516.herokuapp.com/callback'
 }
